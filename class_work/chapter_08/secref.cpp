f90236eca0ef52a6b4d67309773069337ccb5bdb
nvdmike <130056353+nvdmike@users.noreply.github.com>
2025-02-03 22:26:38 +0300

    Update secref.cpp

--- class_work/chapter_08/secref.cpp
+++ class_work/chapter_08/secref.cpp
@@ -4,13 +4,13 @@
 int main() {
 	using namespace std;
 	int rats = 101;
-	int &rodents = rats;								// rodents - это ссылка
+	int &rodents = rats;					// rodents - это ссылка
 	cout << "rats = " << rats;
 	cout << ", rodents = " << rodents << endl;
 	cout << "rats address = " << &rats;
 	cout << ", rodents address = " << &rodents << endl;	// вывод адресов rats и rodents
 	int bunnies = 50;
-	rodents = bunnies;									// можно ли изменить ссылку?
+	rodents = bunnies;					// можно ли изменить ссылку?
 	cout << "bunnies = " << bunnies;
 	cout << ", rats = " << rats;
 	cout << ", rodents = " << rodents << endl;
