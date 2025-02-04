571f1fca7e10ab43a86436ab1858a2cef1695abe
nvdmike <130056353+nvdmike@users.noreply.github.com>
2025-02-04 10:44:47 +0300

    Update mixtypes.cpp

--- class_work/chapter_04/mixtypes.cpp
+++ class_work/chapter_04/mixtypes.cpp
@@ -9,14 +9,14 @@ struct antatcica_years_end {
 int main() {
 	antatcica_years_end s01, s02, s03;
 	s01.year = 1998;
-	antatcica_years_end* pa = &s02;
+	antatcica_years_end *pa = &s02;
 	pa->year = 1999;
 	antatcica_years_end trio[3];	// массив из трех структур
 	trio[0].year = 2003;
 	std::cout << trio->year << std::endl;
-	const antatcica_years_end* arp[3] = { &s01, &s02, &s03 };
+	const antatcica_years_end *arp[3] = { &s01, &s02, &s03 };
 	std::cout << arp[1]->year << std::endl;
-	const antatcica_years_end** ppa = arp;
+	const antatcica_years_end **ppa = arp;
 	auto ppb = arp;			// автоматическое выведение типа в C++
 	// или можно использовать antatcica_years_end **ppb = arp;
 	std::cout << (*ppa)->year << std::endl;
