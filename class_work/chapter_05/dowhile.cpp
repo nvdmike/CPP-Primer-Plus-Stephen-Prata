51d9bd4d17e0072fabb20d7d18332936ce17a256
nvdmike <130056353+nvdmike@users.noreply.github.com>
2025-02-04 09:52:47 +0300

    Update dowhile.cpp

--- class_work/chapter_05/dowhile.cpp
+++ class_work/chapter_05/dowhile.cpp
@@ -7,8 +7,8 @@ int main() {
     cout << "Enter numbers in the range 1-10 to find ";
     cout << "my favorite number\n";	    // запрос на ввод любимого числа из диапазона 1-10
     do {
-        cin >> n;						// выполнить тело
-    } while (n != 7);					// затем проверить
+        cin >> n;					// выполнить тело
+    } while (n != 7);				// затем проверить
     cout << "Yes, 7 is my favorite.\n";	// любимое число - 7
     return 0;
 }
