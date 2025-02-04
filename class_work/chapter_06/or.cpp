f3552792060d4c34ebe3d32822861174fb6cd255
nvdmike <130056353+nvdmike@users.noreply.github.com>
2025-02-04 09:42:53 +0300

    Update or.cpp

--- class_work/chapter_06/or.cpp
+++ class_work/chapter_06/or.cpp
@@ -1,20 +1,20 @@
-// or.cpp -- использование логической операции "ИЛИ"
+// or.cpp -- РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ Р»РѕРіРёС‡РµСЃРєРѕР№ РѕРїРµСЂР°С†РёРё "РР›Р"
 #include <iostream>
-int main()
-{
+
+int main() {
 	using namespace std;
 	cout << "This program may reformat your hard disk\n"
-		 << "and destroy all your data.\n"
-		 << "Do you wish to continue? <y/n> ";
+	     << "and destroy all your data.\n"
+	     << "Do you wish to continue? <y/n> ";
 	char ch;
 	cin >> ch;
-	if ('y' == ch || 'Y' == ch)			// y или Y
+	if ('y' == ch || 'Y' == ch)		// y РёР»Рё Y
 		cout << "You were warned!\a\a\n";
-	else if ('n' == ch || 'N' == ch)	// n или N
+	else if ('n' == ch || 'N' == ch)	// n РёР»Рё N
 		cout << "A wise choice ... bye\n";
 	else
 		cout << "That wasn't a y or n! Apparently you "
-		"can't follow\ninstrunctions, so "
-		"I'll trash your disk anyway.\a\a\a\n";
+			"can't follow\ninstrunctions, so "
+			"I'll trash your disk anyway.\a\a\a\n";
 	return 0;
 }
