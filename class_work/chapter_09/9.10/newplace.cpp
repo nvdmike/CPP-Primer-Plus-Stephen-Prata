330021a79583a9fcfb2f060b748eef71752ee216
nvdmike <130056353+nvdmike@users.noreply.github.com>
2025-02-16 20:03:43 +0300

    Update newplace.cpp

--- class_work/chapter_09/9.10/newplace.cpp
+++ class_work/chapter_09/9.10/newplace.cpp
@@ -1,33 +1,33 @@
-// newplace.cpp -- использование операции new с размещением
+// newplace.cpp -- РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ РѕРїРµСЂР°С†РёРё new СЃ СЂР°Р·РјРµС‰РµРЅРёРµРј
 #include <iostream>
-#include <new>	// для операции new с размещением
+#include <new>	// РґР»СЏ РѕРїРµСЂР°С†РёРё new СЃ СЂР°Р·РјРµС‰РµРЅРёРµРј
 
 const int BUF = 512;
 const int N = 5;
-char buffer[BUF];	// блок памяти
+char buffer[BUF];	// Р±Р»РѕРє РїР°РјСЏС‚Рё
 
 int main() {
 	using namespace std;
 	double *pd1, *pd2;
 	int i;
-	// вызов обычной и операции new с размещением
+	// РІС‹Р·РѕРІ РѕР±С‹С‡РЅРѕР№ Рё РѕРїРµСЂР°С†РёРё new СЃ СЂР°Р·РјРµС‰РµРЅРёРµРј
 	cout << "Calling new and placement new:\n";
-	pd1 = new double[N];							// использование кучи
-	pd2 = new (buffer) double[N];					// использование массива buffer
+	pd1 = new double[N];					// РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ РєСѓС‡Рё
+	pd2 = new (buffer) double[N];				// РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ РјР°СЃСЃРёРІР° buffer
 	for (i = 0; i < N; ++i)
 		pd2[i] = pd1[i] = 1000 + 20.0 * i;
 	cout << "Memory addresses:\n" << "heap: " << pd1
-		 << " static: " << (void *)buffer << endl;	// вывод адресов памяти
-	cout << "Memory contents:\n";					// вывод содержимого памяти
+		 << " static: " << (void *)buffer << endl;	// РІС‹РІРѕРґ Р°РґСЂРµСЃРѕРІ РїР°РјСЏС‚Рё
+	cout << "Memory contents:\n";				// РІС‹РІРѕРґ СЃРѕРґРµСЂР¶РёРјРѕРіРѕ РїР°РјСЏС‚Рё
 	for (i = 0; i < N; ++i) {
 		cout << pd1[i] << " at " << &pd1[i] << "; ";
 		cout << pd2[i] << " at " << &pd2[i] << endl;
 	}
-	// вызов обычной и операции new с размещением во второй раз
+	// РІС‹Р·РѕРІ РѕР±С‹С‡РЅРѕР№ Рё РѕРїРµСЂР°С†РёРё new СЃ СЂР°Р·РјРµС‰РµРЅРёРµРј РІРѕ РІС‚РѕСЂРѕР№ СЂР°Р·
 	cout << "\nCalling new and placement new a second time:\n";
 	double *pd3, *pd4;
-	pd3 = new double[N];							// нахождение нового адреса
-	pd4 = new (buffer) double[N];					// перезаписывание старых данных
+	pd3 = new double[N];					// РЅР°С…РѕР¶РґРµРЅРёРµ РЅРѕРІРѕРіРѕ Р°РґСЂРµСЃР°
+	pd4 = new (buffer) double[N];				// РїРµСЂРµР·Р°РїРёСЃС‹РІР°РЅРёРµ СЃС‚Р°СЂС‹С… РґР°РЅРЅС‹С…
 	for (i = 0; i < N; ++i)
 		pd4[i] = pd3[i] = 1000 + 40.0 * i;
 	cout << "Memory contents:\n";
@@ -35,7 +35,7 @@ int main() {
 		cout << pd3[i] << " at " << &pd3[i] << "; ";
 		cout << pd4[i] << " at " << &pd4[i] << endl;
 	}
-	// вызов обычной и операции new с размещением в третий раз
+	// РІС‹Р·РѕРІ РѕР±С‹С‡РЅРѕР№ Рё РѕРїРµСЂР°С†РёРё new СЃ СЂР°Р·РјРµС‰РµРЅРёРµРј РІ С‚СЂРµС‚РёР№ СЂР°Р·
 	cout << "\nCalling new and placement new a third time:\n";
 	delete[] pd1;
 	pd1 = new double[N];
