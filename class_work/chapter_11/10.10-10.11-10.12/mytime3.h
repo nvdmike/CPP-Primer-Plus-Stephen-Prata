ae377bb4f17327d9d5d8bad262ef6c1f7f1c9a87
nvdmike <130056353+nvdmike@users.noreply.github.com>
2025-04-14 18:28:34 +0300

    Update mytime3.h

--- class_work/chapter_11/10.10-10.11-10.12/mytime3.h
+++ class_work/chapter_11/10.10-10.11-10.12/mytime3.h
@@ -16,9 +16,7 @@ class Time {
 		Time operator+(const Time &t) const;
 		Time operator-(const Time &t) const;
 		Time operator*(double n) const;
-		friend Time operator*(double m, const Time &t) {
-			return t * m;
-		} // встроенное определение
+		friend Time operator*(double m, const Time &t) { return t * m; } // встроенное определение
 		friend std::ostream &operator<<(std::ostream &os, const Time &t);
 };
 
